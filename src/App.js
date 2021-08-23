1440d3c63cc11ddbfb5225fa75606aff8fe9128a
Hama Dler <hamadler48@gmail.com>
2021-08-24 01:13:43 +0300

    layout - responsive navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import { Button } from "antd";
+import React, { useState } from "react";
+import { Layout } from "antd";
+import MainMenu from "./components/MainMenu";
 import "./App.css";
-
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
-
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+function App() {
+  const topics = ["App Description", "App", "Resources"];
+  const [contentIndex, setContentIndex] = useState(0);
+  const [selectedKey, setSelectedKey] = useState("0");
+  const changeSelectedKey = (event) => {
+    const key = event.key;
+    setSelectedKey(key);
+    setContentIndex(+key);
+  };
+  const Menu = (
+    <MainMenu
+      topics={topics}
+      selectedKey={selectedKey}
+      changeSelectedKey={changeSelectedKey}
+    />
+  );
+  return (
+    <div className="App">
+      <NavBar menu={Menu} />
+      <Layout>
+        <SideBar menu={Menu} />
+        <Layout.Content className="content">
+          {topics[contentIndex]}
+        </Layout.Content>
+      </Layout>
+    </div>
+  );
+}
 export default App;
