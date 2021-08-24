e51aeff74e9e7694078e3eb8f0fe623272e56036
Hama Dler <hamadler48@gmail.com>
2021-08-24 03:11:27 +0300

    list

--- src/components/Views/ResourcesView/index.js
+++ src/components/Views/ResourcesView/index.js
@@ -1,52 +1,50 @@
 import React from "react";
 import { Typography, Divider, List } from "antd";
 
-const { Title, Paragraph, Text, Link } = Typography;
+const { Title, Paragraph } = Typography;
 
 const data = [
   {
     title: "Ant Design Documentations",
-    link: "https://reactjs.org/docs/getting-started.html",
+    link: "https://ant.design/docs/react/introduce",
   },
   {
-    title: "Ant Design Title 2",
-    link: "",
-  },
-  {
-    title: "Ant Design Title 3",
-    link: "",
-  },
-  {
-    title: "Ant Design Title 4",
-    link: "",
+    title: "Redirect Default Route",
+    link: "https://reactrouter.com/web/api/Redirect",
   },
 ];
 export default function index() {
   return (
     <div>
       <Title> Resources View</Title>
-
+      <Paragraph style={{ marginBottom: "4rem" }}>
+        There are the resources that were made use of in the development of this
+        example app
+      </Paragraph>
       <List
         itemLayout="horizontal"
         dataSource={data}
         renderItem={(item) => (
-          <List.Item
-            actions={[
-              <a
-                href={item.link}
-                key="list-loadmore-edit"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Visit
-              </a>,
-            ]}
-          >
-            <List.Item.Meta
-              title={<a href={item.link}>{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
+          <>
+            <List.Item
+              actions={[
+                <a
+                  href={item.link}
+                  key="list-loadmore-edit"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Visit
+                </a>,
+              ]}
+            >
+              <List.Item.Meta
+                title={<a href={item.link}>{item.title}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              />
+            </List.Item>
+            <Divider />
+          </>
         )}
       />
     </div>
