8b3a57dca05981062037d549b367a87100c71cf0
Hama Dler <hamadler48@gmail.com>
2021-08-26 02:35:14 +0300

    spinner component

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Select, Row, Col, List, Button, Divider } from "antd";
+import { Select, Row, Col, List, Button, Divider, Spin } from "antd";
 
 const { Option } = Select;
 
@@ -9,7 +9,7 @@ class UserList extends Component {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false,
+      isLoading: false,
       posts: [],
     };
 
@@ -17,12 +17,14 @@ class UserList extends Component {
     this.axiosCall = this.axiosCall.bind(this);
   }
 
+  // Function that takes in User ID, makes an api call to get posts for that user and updates the state
   axiosCall(userId) {
+    this.setState({ isLoading: true });
     axios
       .get(`https://jsonplaceholder.typicode.com/posts/${userId}`)
       .then((response) => {
         this.setState({
-          isLoaded: true,
+          isLoading: false,
           posts: response.data,
         });
         console.log(response.data);
@@ -30,10 +32,9 @@ class UserList extends Component {
   }
 
   componentDidMount() {
-    // send HTTP request
-    // save it to the state
-    this.axiosCall(1);
-    console.log("user list component mounted");
+    // Populating state with posts from user of ID 1 on component mount
+    // this.axiosCall(1);
+    console.log("Posts for the first user list have been loaded");
   }
 
   handleChange(userId) {
@@ -46,7 +47,7 @@ class UserList extends Component {
       <>
         <Select
           defaultValue="Pick User"
-          style={{ width: 120 }}
+          style={{ width: 140, marginBottom: "4rem" }}
           onChange={this.handleChange}
         >
           <Option value="1">User with ID 1</Option>
@@ -71,30 +72,38 @@ class UserList extends Component {
 
         <Row>
           <Col span={24}>
-            <List
-              itemLayout="horizontal"
-              dataSource={[this.state.posts]}
-              renderItem={(post) => (
-                <>
-                  <List.Item
-                    actions={[
-                      <Button type="primary" key="list-loadmore-edit">
-                        Edit
-                      </Button>,
-                      <Button danger key="list-loadmore-edit">
-                        Delete
-                      </Button>,
-                    ]}
-                  >
-                    <List.Item.Meta
-                      title={<a href="https://ant.design">{post.title}</a>}
-                      description={post.body}
-                    />
-                  </List.Item>
-                  <Divider></Divider>
-                </>
-              )}
-            />
+            {this.state.isLoading ? (
+              <Spin tip="Loading..." size="large" style={{ height: "50%" }}>
+                <div style={{ background: "#f0f2f5", height: 500, margin: 24 }}>
+                  Loading
+                </div>
+              </Spin>
+            ) : (
+              <List
+                itemLayout="horizontal"
+                dataSource={[this.state.posts]}
+                renderItem={(post) => (
+                  <>
+                    <List.Item
+                      actions={[
+                        <Button type="primary" key="list-loadmore-edit">
+                          Edit
+                        </Button>,
+                        <Button danger key="list-loadmore-edit">
+                          Delete
+                        </Button>,
+                      ]}
+                    >
+                      <List.Item.Meta
+                        title={<a href="https://ant.design">{post.title}</a>}
+                        description={post.body}
+                      />
+                    </List.Item>
+                    <Divider></Divider>
+                  </>
+                )}
+              />
+            )}
           </Col>
         </Row>
       </>
